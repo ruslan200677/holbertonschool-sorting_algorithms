36800c778c130ff076dcb336c023d9041e4b9e56
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-08-10 08:56:15 +0000

    Selection Sorting

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,34 +1,36 @@
-#include <stdio.h>
-#include <stddef.h>
 #include "sort.h"
 
 /**
- * selection_sort - Sorts an array of integers in asc order using
- * the Selection sort algorithm
- * @array: Pointer to the first element of the array
- * @size: Number of elements in the array
+ * selection_sort -  a function that sorts an array of integers.
+ * @array : array.
+ * @size : size of array.
+ * Return : sorted array
+ *
  */
 
 void selection_sort(int *array, size_t size)
 {
-size_t i, j, min_idx;
-int temp;
-if (array == NULL || size < 2)
-return;
-for (i = 0; i < size - 1; i++)
-{
-min_idx = i;
-for (j = i + 1; j < size; j++)
-{
-if (array[j] < array[min_idx])
-min_idx = j;
+	size_t i, j, min;
+
+	int tmp;
+
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+		if (min != i)
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+			print_array(array, size)
+		}
+	}
 }
-if (min_idx != i)
-{
-temp = array[i];
-array[i] = array[min_idx];
-array[min_idx] = temp;
-print_array(array, size);
-}
-}
-}
+
